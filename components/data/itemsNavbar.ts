394f670dbf04dca15830aaa5bb479de93bd08863
Gerardo-Rioss <gerarios26@gmail.com>
2025-12-11 12:02:55 -0300

    update Navbar Desktop

---
+++ components/data/itemsNavbar.ts
@@ -0,0 +1,22 @@
+export const itemsNavbar=[
+    {
+        name:"Inicio",
+        Link:"/"
+    },
+    {
+        name:"Series",
+        Link:"/series"
+    },
+    {
+        name:"Películas",
+        Link:"/movies"
+    },
+    {
+        name:"Novedades mas vistas",
+        Link:"/more-views"
+    },
+    {
+        name:"Mi lista",
+        Link:"/my-list"
+    },
+]
