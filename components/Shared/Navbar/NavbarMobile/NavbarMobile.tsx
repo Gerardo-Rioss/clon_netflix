b28af25a442705cd41a6965ef3795667d9e7a4df
Gerardo-Rioss <gerarios26@gmail.com>
2025-12-11 12:58:52 -0300

    update Navbar Mobile

--- components/Shared/Navbar/NavbarMobile/NavbarMobile.tsx
+++ components/Shared/Navbar/NavbarMobile/NavbarMobile.tsx
@@ -1,7 +1,36 @@
+import { itemsNavbar } from "@/components/data/itemsNavbar"
+import Logo from "@/components/Shared/Logo/Logo"
+import Link from "next/link"
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet"
+import { Menu } from "lucide-react"
+
+
 const NavbarMobile = () => {
   return (
-    <div>
-      NavbarMobile
+    <div className="p-4 flex justify-between">
+      <Logo/>
+      <Sheet>
+  <SheetTrigger>
+    <Menu/>
+  </SheetTrigger>
+  <SheetContent side="left" className="bg-black">
+    <div className="flex flex-col gap-4">
+      {itemsNavbar.map((item)=>(
+        <Link key={item.name} href={item.Link} className="hover:text-gray-300 transition-all duration-300">
+          {item.name}
+        </Link>
+      ))}
+    </div>
+    <div className="border-[1px] border-white/70 my-5"/>
+  </SheetContent>
+</Sheet>
     </div>
   )
 }
