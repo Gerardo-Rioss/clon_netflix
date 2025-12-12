2d8c2cd8e137c5005481fa7de74b58dabb315cdb
Gerardo-Rioss <gerarios26@gmail.com>
2025-12-12 08:55:07 -0300

    agregado login page

--- components/Shared/Navbar/NavbarMobile/NavbarMobile.tsx
+++ components/Shared/Navbar/NavbarMobile/NavbarMobile.tsx
@@ -1,15 +1,8 @@
 import { itemsNavbar } from "@/components/data/itemsNavbar"
+import { BellRing, Menu, Search } from "lucide-react"
 import Logo from "@/components/Shared/Logo/Logo"
 import Link from "next/link"
-import {
-  Sheet,
-  SheetContent,
-  SheetDescription,
-  SheetHeader,
-  SheetTitle,
-  SheetTrigger,
-} from "@/components/ui/sheet"
-import { Menu } from "lucide-react"
+import {Sheet,SheetContent,SheetTrigger} from "@/components/ui/sheet"
 
 
 const NavbarMobile = () => {
@@ -21,7 +14,7 @@ const NavbarMobile = () => {
     <Menu/>
   </SheetTrigger>
   <SheetContent side="left" className="bg-black">
-    <div className="flex flex-col gap-4">
+    <div className="flex flex-col gap-4 mt-1 ml-1">
       {itemsNavbar.map((item)=>(
         <Link key={item.name} href={item.Link} className="hover:text-gray-300 transition-all duration-300">
           {item.name}
@@ -29,6 +22,14 @@ const NavbarMobile = () => {
       ))}
     </div>
     <div className="border-[1px] border-white/70 my-5"/>
+    <div className="flex justify-between gap-6 mt-4 ml-1 mr-1">
+      <Search className="cursor-pointer"/>
+      <BellRing className="cursor-pointer"/>
+      
+      <p>User</p>
+      {/* TODO: add user profile */}
+
+    </div>
   </SheetContent>
 </Sheet>
     </div>
