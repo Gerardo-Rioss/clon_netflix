637d42af16872a2e98eff7618066130b11baafd6
Gerardo-Rioss <gerarios26@gmail.com>
2025-12-30 11:59:57 -0300

    funcional prisma y neon

--- lib/db.ts
+++ lib/db.ts
@@ -1,45 +1,10 @@
-/* import "dotenv/config";
+import "dotenv/config";
 import { PrismaPg } from '@prisma/adapter-pg'
-import { PrismaClient } from '../generated/prisma/client'
+import { PrismaClient } from './generated/prisma/client'
 
 const connectionString = `${process.env.DATABASE_URL}`
 
 const adapter = new PrismaPg({ connectionString })
 const db = new PrismaClient({ adapter })
 
-export { db } */
-
-/* import { PrismaClient } from "@/generated/prisma/client";
-const globalForPrisma = global as unknown as {prisma: PrismaClient};
-export const db = globalForPrisma.prisma || new PrismaClient();
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = db; */
-
-/* import "dotenv/config";
-import { PrismaPg } from '@prisma/adapter-pg'
-import { PrismaClient } from '../generated/prisma/client'
-
-const connectionString = `${process.env.DATABASE_URL}`
-
-const adapter = new PrismaPg({ connectionString })
-const db = new PrismaClient({ adapter })
-
-export { db } */
-
-import { PrismaClient } from '../generated/prisma/client'
-import { PrismaPg } from '@prisma/adapter-pg'
-
-const globalForPrisma = global as unknown as {
-    prisma: PrismaClient
-}
-
-const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL,
-})
-
-const db = globalForPrisma.prisma || new PrismaClient({
-  adapter,
-})
-
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = db
-
-export default db
+export { db }
