e1dde7684ace48fc83048d5c24b5462061dbf5f8
Gerardo-Rioss <gerarios26@gmail.com>
2025-12-30 12:12:59 -0300

    inicializado auth js

--- auth.ts
+++ auth.ts
@@ -1,16 +1,5 @@
 import NextAuth from "next-auth"
-import {PrismaAdapter} from "@auth/prisma-adapter"
-/* import db from "./lib/db" */
+ 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  /* adapter: PrismaAdapter(db), */
   providers: [],
 })
-
- 
-/* import NextAuth from "next-auth"
-import { PrismaAdapter } from "@auth/prisma-adapter"
-import db from "./lib/db" 
-export const { handlers, auth, signIn, signOut } = NextAuth({
-  adapter: PrismaAdapter(db),
-  providers: [],
-}) */
